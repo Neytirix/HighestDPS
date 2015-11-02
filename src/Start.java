3ce2a2d8ff8f5dde4a4a9db448f3b5d6958ecede
Neytirix <lxhworks@vip.qq.com>
2015-11-01 21:48:21 -0500

    Initial Commit.

---
+++ src/Start.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Neytirix
+ *
+ */
+public class Start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/1?champData=spells&api_key=4054d80e-2a76-419d-a53d-773001317c76";
+		
+	}
+
+}
